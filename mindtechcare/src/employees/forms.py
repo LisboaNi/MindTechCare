b733b107d3a0cfe4f544bd9319f612fd5a3846cd
Nicoli Lisboa <nick.lisboa04@gmail.com>
2025-04-05 17:12:37 -0300

    Done: Ajuste Tokens e Senhas na edição n mostra

--- mindtechcare/src/employees/forms.py
+++ mindtechcare/src/employees/forms.py
@@ -5,18 +5,32 @@ from django.contrib.auth.forms import AuthenticationForm
 
 
 class EmployeeForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Digite sua nova senha'}),
+        label="Senha"
+    )
+
     class Meta:
         model = Employee
         fields = ['name', 'email', 'password', 'function']
 
+
 class EmployeeLoginForm(AuthenticationForm):
-    pass 
+    pass
+
 
 class TokenForm(forms.ModelForm):
+    trello_token = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Cole aqui seu  novo token do Trello'}),
+        label="Token Trello"
+    )
+    github_token = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(attrs={'placeholder': 'Cole aqui seu novo token do GitHub'}),
+        label="Token GitHub"
+    )
+
     class Meta:
         model = Employee
-        fields = ['trello_username','trello_token', 'github_username', 'github_token']
-        widgets = {
-            'trello_token': forms.TextInput(attrs={'placeholder': 'Cole aqui seu token do Trello'}),
-        }
+        fields = ['trello_username', 'trello_token', 'github_username', 'github_token']
